4136033d2d72a7dae6d7e505617509c47d0c7b8c
Devon <k849996781@vip.qq.com>
2017-08-23 11:32:37 +0800

    changes

---
+++ api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "apidoc-CHIHU2",
+  "version": "0.3.0",
+  "description": "Welcome to apiDoc",
+  "title": "chihu2 apiDoc",
+  "url": "https://devonhello.com/chihu_v3",
+  "sampleUrl": "https://devonhello.com/chihu_v3",
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-08-23T03:27:56.051Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
